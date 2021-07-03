71e14525ee9419ea059015931f553c695ffdc155
James Butcher <jamesbutcher167@gmail.com>
2021-07-03 15:30:53 +0100

    Add base functionality

--- main.nim
+++ main.nim
@@ -1,4 +1,4 @@
-import httpclient, json, nancy
+import httpclient, json, nancy, strutils
 
 let client = httpclient.newHttpClient()
 
@@ -9,19 +9,43 @@ iterator items(start: int, final: int): int =
     yield curr
     inc curr
 
-proc void(qwery: string): JsonNode =
-  return parseJson(client.request("https://xq-api.voidlinux.org/v1/query/x86_64?q=gcc").body)["data"]
+type
+  Package = object
+    index: string
+    name: string
+    description: string
+    version: string
+
+  Query = object
+    results: seq[Package]
+    url: string
+
+proc newPackage(index: string, name: string, description: string, version: string): Package =
+  result.index = index
+  result.name = name
+  result.description = description
+  result.version = version
+
+proc void(query: string): Query =
+
+  let
+    url = "https://xq-api.voidlinux.org/v1/query/x86_64?q=$#" % [query]
+    data: JsonNode = parseJson(client.request(url).body)["data"]
+
+  for i in items(0, data.len):
+    result.results.add newPackage($(i+1), data[i]["name"].getStr, data[i]["short_desc"].getStr, data[i]["version"].getStr)
 
 proc main() =
-  var data: JsonNode = void("gcc")
-  var table: TerminalTable
+  var
+    data: Query = void("gcc")
+    table: TerminalTable
 
   table.add "Index", "Package", "Description", "Version"
 
-  for i in items(0, data.len):
-    table.add $i, data[i]["name"].getStr, data[i]["short_desc"].getStr, data[i]["version"].getStr
+  for i in data.results:
+    table.add i.index, i.name, i.description, i.version
 
   table.echoTableSeps(seps = boxSeps)
 
-
-main()
+with isMainModule:
+  main()
