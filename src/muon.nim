b052a12ea413735fadcbb6c236d48d1ba0dd8971
James Butcher <jamesbutcher167@gmail.com>
2021-07-04 19:48:17 +0100

    Add command line arguments

--- src/muon.nim
+++ src/muon.nim
@@ -1,6 +1,15 @@
-import fetcher
+import fetcher, argparse, tables
+
+let functionTranslationTable = {"void": xq, "aur": aur}.toTable
+
+let p = newParser:
+  option("-r", "--repo", help="Repository")
+  arg("package")
 
 proc main() =
-  createTable(void("gcc"))
+  let args = p.parse(commandLineParams())
+
+  let api = functionTranslationTable[args.repo]
+  createTable(api(args.package))
 
 main()
