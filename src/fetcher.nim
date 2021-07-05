1ab94f5b3a0213f55a25d346426e862aa5ca111a
James Butcher <jamesbutcher167@gmail.com>
2021-07-05 17:36:28 +0100

    Add ProtoPackage system and exactMatch

--- src/fetcher.nim
+++ src/fetcher.nim
@@ -1,4 +1,4 @@
-import httpclient, json, nancy, strutils, argparse
+import httpclient, json, nancy, strutils, argparse, sequtils
 
 let client = httpclient.newHttpClient()
 
@@ -10,15 +10,22 @@ iterator items(start: int, final: int): int =
     inc curr
 
 type
-  Package* = object
-    index: string
+  ProtoPackage = object of RootObj
     name: string
     description: string
     version: string
 
+  Package* = object of ProtoPackage
+    index: string
+
   Query* = object
     results: seq[Package]
     url: string
+
+proc newProtoPackage(name: string, description: string, version: string): ProtoPackage =
+  result.name = name
+  result.description = description
+  result.version = version
 
 proc newPackage(index: string, name: string, description: string, version: string): Package =
   result.index = index
@@ -32,14 +39,29 @@ proc getMax(data: JsonNode, largest: int): int =
 
   return data.len
 
+proc exactMatch(packageList: var seq[ProtoPackage], packageName: string) =
+
+  for index, package in packageList:
+    if package.name == packageName:
+      packageList.insert(@[packageList[index]], 0)
+      packageList.delete(index+1)
+      return
+
 proc xq*(query: string): Query =
 
   let
     url = "https://xq-api.voidlinux.org/v1/query/x86_64?q=$#" % [query]
     data: JsonNode = parseJson(client.request(url).body)["data"]
 
-  for i in items(0, getMax(data, 10)):
-    result.results.add newPackage($(i+1), data[i]["name"].getStr, data[i]["short_desc"].getStr, data[i]["version"].getStr)
+  var packageList: seq[ProtoPackage]
+
+  for index, item in data.getElems():
+    packageList.add newProtoPackage(item["name"].getStr, item["short_desc"].getStr, item["version"].getStr)
+
+  packageList.exactMatch(query)
+
+  for index, item in packageList:
+    result.results.add newPackage($(index+1), item.name, item.description, item.version)
 
 proc aur*(query: string): Query =
 
