6c734292b44eb9475729c8e5a275e304e73931be
James Butcher <jamesbutcher167@gmail.com>
2021-07-06 22:46:35 +0100

    Add items option

--- src/muon.nim
+++ src/muon.nim
@@ -3,13 +3,13 @@ import fetcher, argparse, tables
 let functionTranslationTable = {"void": xq, "aur": aur}.toTable
 
 let p = newParser:
-  option("-r", "--repo", help="Repository")
+  option("-r", "--repo", help="Repository to search")
+  option("-i", "--items", default=some("10"), help="Number of possibilities to display")
   arg("package")
 
 proc main() =
   let args = p.parse(commandLineParams())
-
   let api = functionTranslationTable[args.repo]
-  createTable(api(args.package))
+  createTable(api(args.package, args.items.parseInt()))
 
 main()
